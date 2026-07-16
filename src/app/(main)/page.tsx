df623b99bfd0efb63cffb56a6c81580629911f78
aek <aekthan.b@gmail.com>
2026-07-17 01:01:22 +0700

    refactor: adjust layout properties for improved responsiveness in Home component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,7 +2,7 @@ import { EditorialBlogSection } from "./_components/editorial-blog-section";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col items-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex w-full flex-1 flex-col items-center overflow-x-hidden bg-zinc-50 font-sans dark:bg-black">
       <EditorialBlogSection />
     </div>
   );
